d96e38753871260b4b7c118d3ea46b4b11297603
Matias <matiasruiz2015@outlook.com>
2025-02-25 19:45:33 -0300

    Forzar carga manual de KeySecurity.dll

--- Program.cs
+++ Program.cs
@@ -1,17 +1,3 @@
-using System;
-using System.IO;
-using System.Reflection;
-
-AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-{
-    string dllPath = Path.Combine(AppContext.BaseDirectory, "lib", "KeySecurity.dll");
-    if (File.Exists(dllPath))
-    {
-        return Assembly.LoadFrom(dllPath);
-    }
-    return null;
-};
-
 var builder = WebApplication.CreateBuilder(args);
 
 AppContext.SetSwitch("System.Text.Json.Serialization.JsonSerializerDefaults", true);
@@ -20,6 +6,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();  // <-- IMPORTANTE: Esto debe estar para que los controladores funcionen
 
 app.Run();
