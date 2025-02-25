b546f39a069a5cbe0edfdc7d82a6ac5bcc1be952
Matias <matiasruiz2015@outlook.com>
2025-02-25 19:39:47 -0300

    Solución para cargar KeySecurity.dll en Render

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+ppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+{
+    string dllPath = Path.Combine(AppContext.BaseDirectory, "lib", "KeySecurity.dll");
+    return File.Exists(dllPath) ? Assembly.LoadFrom(dllPath) : null;
+};
+
+
 AppContext.SetSwitch("System.Text.Json.Serialization.JsonSerializerDefaults", true);
 builder.Services.AddControllers();  // <-- Asegúrate de que esta línea está
 var app = builder.Build();
